cb30234b5c645de977d6d304422d715f4f9b7470
Anar Bayramov <abayramov@sportmasterlab.net>
2023-06-12 18:42:17 +0300

    Добавлена сортировка и поправлена пагинация

--- src/components/MainPage/MainPage.vue
+++ src/components/MainPage/MainPage.vue
@@ -1,50 +1,97 @@
 <template>
   <div class="main-page">
-    <header class="main-page__header">
-      <h1 class="main-page__header-title">
+    <header class="main-page__title">
+      <h1 class="main-page__title-text">
         Список пользователей
       </h1>
       <button
-        class="btn main-page__header-button waves-effect waves-light"
+        class="btn waves-effect waves-light"
         :class="{'disabled': loading}"
         @click="showDialog"
+        tabindex="0"
       >
         Добавить
       </button>
     </header>
 
-    <table
-      class="main-page__table striped"
+    <div
+      class="main-page__table"
     >
-      <thead class="main-page__table-head">
-        <tr>
-          <th>ID</th>
-          <th>Фамилия</th>
-          <th>Имя</th>
-          <th>Дата рождения</th>
-        </tr>
-      </thead>
+      <div class="main-page__header">
+        <div
+          class="main-page__table-id main-page__header-item"
+          @click="sortByType('id')"
+          @keypress.enter="sortByType('id')"
+          tabindex="0"
+        >
+          <div>ID</div>
+          <TaSortIcon
+            :active="isSortById"
+            :reverse="isSortById && sortReverse"
+            class="main-page__sort"
+          />
+        </div>
+        <div
+          class="main-page__table-lastname main-page__header-item"
+          @click="sortByType('ln')"
+          @keypress.enter="sortByType('ln')"
+          tabindex="0"
+        >
+          <div>Фамилия</div>
+          <TaSortIcon
+            :active="isSortByLastName"
+            :reverse="isSortByLastName && sortReverse"
+            class="main-page__sort"
+          />
+        </div>
+        <div
+          class="main-page__table-firstname main-page__header-item"
+          @click="sortByType('fn')"
+          @keypress.enter="sortByType('fn')"
+          tabindex="0"
+        >
+          <div>Имя</div>
+          <TaSortIcon
+            :active="isSortByFirstName"
+            :reverse="isSortByFirstName && sortReverse"
+            class="main-page__sort"
+          />
+        </div>
+        <div
+          class="main-page__table-birthday"
+          @click="sortByType('bd')"
+          @keypress.enter="sortByType('bd')"
+          tabindex="0"
+        >
+          <div>Дата рождения</div>
+        </div>
+      </div>
 
-      <tbody
+      <div
         class="main-page__table-body"
         :class="{'main-page__table-body--loading': loading}"
       >
-        <tr v-for="user in slicedUsers" :key="user.id">
-          <th>{{ user.id }}</th>
-          <th>{{ user.lastName }}</th>
-          <th>{{ user.firstName }}</th>
-          <th>{{ user.birthday }}</th>
-        </tr>
-      </tbody>
+        <div
+          v-for="(user, index) in modelUsers"
+          class="main-page__table-body-item"
+          :key="`${user.id}_${index}`"
+        >
+          <span class="main-page__table-id">{{ user.id }}</span>
+          <span class="main-page__table-lastname">{{ user.lastName }}</span>
+          <span class="main-page__table-firstname">{{ user.firstName }}</span>
+          <span class="main-page__table-birthday">{{ user.birthday }}</span>
+        </div>
+      </div>
 
       <TaLoader v-if="loading" class="main-page__loader" />
-    </table>
+    </div>
 
     <div class="main-page__pagination">
       <button
         class="btn-small"
-        :class="{'disabled': users.length <= pagination.endAt || loading}"
+        :class="{'disabled': users.length <= endAt || loading}"
         type="button"
+        tabindex="0"
         @click="paginationPlus"
       >
         Загрузить ещё
@@ -63,6 +110,7 @@
 <script>
 import TaLoader from '@/components/TaLoader';
 import TaDialog from '@/components/TaDialog';
+import TaSortIcon from '@/components/TaSortIcon';
 import { initialUsers } from './constants';
 
 export default {
@@ -70,24 +118,53 @@ export default {
   components: {
     TaLoader,
     TaDialog,
+    TaSortIcon,
   },
 
   data() {
     return {
-      users: [{}, {}, {}, {}, {}, {}],
-      pagination: {
-        startAt: 0,
-        endAt: 5,
-      },
+      users: [{}, {}, {}, {}, {}],
+      modelUsers: [{}, {}, {}, {}, {}],
+      startAt: 0,
+      endAt: 5,
       loading: true,
       dialogShown: false,
+      // Сортировка
+      sortType: 'id',
+      sortReverse: false,
     };
   },
 
+  watch: {
+    users: {
+      handler() {
+        this.fillModel();
+      },
+      immediate: true,
+    },
+    endAt() {
+      this.fillModel();
+    },
+    sortType() {
+      this.sortModel();
+    },
+    sortReverse() {
+      this.sortModel();
+    },
+  },
+
   computed: {
-    slicedUsers() {
-      const { startAt, endAt } = this.pagination;
-      return this.users.slice(startAt, endAt);
+    isSortById() {
+      const { sortType } = this;
+      return sortType === 'id';
+    },
+    isSortByLastName() {
+      const { sortType } = this;
+      return sortType === 'ln';
+    },
+    isSortByFirstName() {
+      const { sortType } = this;
+      return sortType === 'fn';
     },
   },
 
@@ -130,9 +207,58 @@ export default {
       this.loading = true;
 
       setTimeout(() => {
-        this.pagination.endAt += 5;
+        this.endAt += 5;
         this.loading = false;
       }, 500);
+    },
+    sortByType(type) {
+      if (this.sortType === type) {
+        this.sortReverse = !this.sortReverse;
+      } else {
+        this.sortReverse = false;
+        this.sortType = type;
+      }
+    },
+    // Заполняет модель согласно пагинации
+    fillModel() {
+      const { startAt, endAt } = this;
+      this.modelUsers = this.users.slice(startAt, endAt);
+    },
+    // Сортирует модель соглано типа сортировки
+    sortModel() {
+      const {
+        isSortById, isSortByLastName, isSortByFirstName, sortReverse,
+      } = this;
+
+      if (isSortById) {
+        if (sortReverse) {
+          this.modelUsers.sort((a, b) => +b.id - +a.id);
+        } else {
+          this.modelUsers.sort((a, b) => +a.id - +b.id);
+        }
+      }
+
+      if (isSortByLastName) {
+        if (sortReverse) {
+          this.modelUsers.sort((a, b) => this.getSortValueByChar(b.lastName, a.lastName));
+        } else {
+          this.modelUsers.sort((a, b) => this.getSortValueByChar(a.lastName, b.lastName));
+        }
+      }
+
+      if (isSortByFirstName) {
+        if (sortReverse) {
+          this.modelUsers.sort((a, b) => this.getSortValueByChar(b.firstName, a.firstName));
+        } else {
+          this.modelUsers.sort((a, b) => this.getSortValueByChar(a.firstName, b.firstName));
+        }
+      }
+    },
+    getSortValueByChar(a, b) {
+      if (a.charAt(0) < b.charAt(0)) {
+        return -1;
+      }
+      return 1;
     },
   },
 
---
+++ src/components/TaSortIcon/TaSortIcon.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="ta-sort-icon"
+    :class="{'ta-sort-icon--active': active}"
+  >
+    <i class="material-icons">sort</i>
+    <i
+      class="material-icons dropdown"
+      :class="{'reverse': reverse}"
+    >
+      arrow_drop_down
+    </i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaSortIcon',
+
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    reverse: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import './styles.scss';
+</style>
